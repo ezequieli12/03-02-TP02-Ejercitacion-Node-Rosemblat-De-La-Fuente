5e7a0fa15cbd9bd238c81fcb5ee1e6dc459a8314
Android Studio <devandroid@ort>
2025-03-31 10:32:33 -0300

    IJIJ

--- src/ej04.js
+++ src/ej04.js
@@ -3,5 +3,7 @@ const ARCHIVO_ENTRADA = "./archivo-entrada.txt";
 const ARCHIVO_SALIDA = "./archivo-salida.txt";
 console.clear();
 copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
-function copiar(origen, destino){
+function copiar(origen, destino)
+{
+    
 }
---
+++ src/ej05.js
@@ -0,0 +1,9 @@
+let miUrl    = null;
+let miObjeto = null;
+miUrl = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
+miObjeto = parsearUrl (miUrl);
+console.log(miObjeto);
+function parsearUrl(laURL)
+{
+    // No seas vago, acá hay que hacer el cuerpo de la función!!!
+}
